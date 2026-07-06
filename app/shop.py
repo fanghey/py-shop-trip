15be1606505c5386b741f79368fe36dd801917d2
fanghey <2603Lysenko2603@gmail.com>
2026-07-06 12:28:57 +0200

    Update shop.py

--- app/shop.py
+++ app/shop.py
@@ -1,14 +1,28 @@
+from datetime import datetime
+
+
 class Shop:
-    def __init__(self, name: str, location: list, products: dict) -> None:
+    def __init__(self, name, location, products):
         self.name = name
         self.location = location
         self.products = products
 
-    def __str__(self) -> str:
-        return f"{self.name}"
+    def calculate_products_cost(self, cart: dict):
+        total = 0
+        for item, qty in cart.items():
+            total += self.products[item] * qty
+        return total
 
-    def calculate_cart_price(self, products_cart: dict) -> float:
-        cart_price = 0
-        for product, quantity in products_cart.items():
-            cart_price += self.products[product] * quantity
-        return cart_price
+    def print_receipt(self, customer_name, cart: dict, total_cost: float):
+        current_time = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+
+        print(f"Date: {current_time}")
+        print(f"Thanks, {customer_name}, for your purchase!")
+        print("You have bought:")
+
+        for item, qty in cart.items():
+            price = self.products[item] * qty
+            print(f"{qty} {item}s for {price:.2f} dollars")
+
+        print(f"Total cost is {total_cost:.2f} dollars")
+        print("See you again!")
